e2121f666e55c3bd87c31200955ea9ef945cdb97
Peter Hofmann <phofmann@seibert-media.net>
2020-10-13 07:36:55 +0200

    Revert "Merge pull request #3 in SMEDIA/fluesterfix from fku-das-mit-den-passwoertern to master"
    This reverts commit aa2b9b459d1ea5b7d37ef6e22a8d6728da21c89c, reversing
    changes made to aabdcb73130339be896f23e805c6d0f2d161a6e9.

--- fluesterfix/__init__.py
+++ fluesterfix/__init__.py
@@ -4,7 +4,7 @@
 from base64 import b64decode, b64encode
 from random import choice
 from os import environ, mkdir, rename
-from os.path import isfile, join
+from os.path import join
 from re import compile as re_compile
 from shutil import rmtree
 from string import ascii_letters, digits
@@ -54,7 +54,7 @@ def html(body):
 </html>'''
 
 
-def retrieve(sid, password=None):
+def retrieve(sid):
     # Try to rename this sid's directory. This is an atomic operation on
     # POSIX file systems, meaning two concurrent requests cannot rename
     # the same directory -- for one of them, it will look like the
@@ -66,20 +66,6 @@ def retrieve(sid, password=None):
     except OSError:
         return None
 
-    # If the secret requires a password, check it now. This is done
-    # after the secret has been locked. This will remove the secret if
-    # a wrong password is supplied, thus removing any ability to try
-    # brute-forcing a password, because the allowed number of attempts
-    # is limited to 1 (one).
-    passwordfile = join(DATA, locked_sid, 'password')
-    if password and isfile(passwordfile):
-        with open(passwordfile, 'rb') as fp:
-            password_bytes = fp.read()
-
-        if password != decrypt(password_bytes):
-            rmtree(join(DATA, locked_sid))
-            return None
-
     # Now that we have "locked" this sid, we can safely read it and then
     # destroy it.
     with open(join(DATA, locked_sid, 'secret'), 'rb') as fp:
@@ -89,7 +75,7 @@ def retrieve(sid, password=None):
     return decrypt(secret_bytes)
 
 
-def store(secret, password=None):
+def store(secret):
     while True:
         try:
             # Again, mkdir is an atomic operation on POSIX file systems.
@@ -100,10 +86,6 @@ def store(secret, password=None):
             break
         except FileExistsError:
             continue
-
-    if password:
-        with open(join(DATA, sid, 'password'), 'wb') as fp:
-            fp.write(encrypt(password))
 
     with open(join(DATA, sid, 'secret'), 'wb') as fp:
         fp.write(encrypt(secret))
@@ -123,8 +105,6 @@ def index():
            you will get a link that you can send to someone else. That
            link can only be used once.</p>
         <form action="/new" method="post">
-            <input type="password" name="password"
-             placeholder="If you want to secure the secret using an additional password, enter it here.">
             <textarea name="data"></textarea>
             <input type="submit" value="&#x1f517; Create link">
         </form>
@@ -134,16 +114,14 @@ def index():
 @app.route('/new', methods=['POST'])
 def new():
     try:
-        form = request.form.to_dict()
-        secret = form['data']
-        password = form['password']
+        secret = request.form.to_dict()['data']
     except:
         return 'Garbage'
 
     if len(secret.strip()) <= 0:
         return redirect(url_for('index'))
 
-    sid = store(secret, password)
+    sid = store(secret)
     scheme = request.headers.get('x-forwarded-proto', 'http')
     host = request.headers.get('x-forwarded-host', request.headers['host'])
     sid_url = f'{scheme}://{host}/get/{sid}'
@@ -160,20 +138,11 @@ def get(sid):
     validate_sid(sid)
     # FIXME Without that hidden field, lynx insists on doing GET. Is
     # that a bug in lynx or is it invalid to POST empty forms?
-    form_field = '<input name="compat" type="hidden" value="lynx needs this">'
-    if isfile(join(DATA, sid, 'password')):
-        form_field = (
-            '<p><label for="password">This secret is protected by a password. '
-            'Please enter it here:</label></p>'
-            '<input type="password" name="password" id="password">'
-            '<p>Be careful! If you enter a wrong password, the secret '
-            'will be gone forever.</p>'
-        )
     return html(f'''
         <h1>Reveal this secret?</h1>
         <p>You can only do this once.</p>
         <form action="/reveal/{sid}" method="post">
-            {form_field}
+            <input name="compat" type="hidden" value="lynx needs this">
             <input type="submit" value="&#x1f50d; Reveal the secret">
         </form>
     ''')
@@ -182,15 +151,11 @@ def get(sid):
 @app.route('/reveal/<sid>', methods=['POST'])
 def reveal(sid):
     validate_sid(sid)
-    try:
-        password = request.form.to_dict()['password']
-    except:
-        password = None
-    secret = retrieve(sid, password)
+    secret = retrieve(sid)
     if secret is None:
         return html(f'''
             <h1>Error</h1>
-            <p>This secret could not be retrieved.</p>
+            <p>This secret has already been revealed.</p>
         '''), 404
     else:
         # Show all lines, if possible. Never show more than 100, though.
